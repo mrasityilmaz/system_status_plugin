8b6b57db93679ab84b9e0eb95f957fbec00a949b
Muhammed Raşit Yılmaz <rasityilmaz@yaani.com>
2024-06-28 18:00:03 +0300

    library-updated

--- system_status/lib/system_status.dart
+++ system_status/lib/system_status.dart
@@ -1,6 +1,7 @@
 library system_status;
 
 export 'package:system_status/system_status.dart';
+export 'package:system_status_macos/system_status_macos.dart';
 export 'package:system_status_platform_interface/src/Enums/system_status_type.dart';
 export 'package:system_status_platform_interface/src/Models/battery_status/battery_status_model.dart';
 export 'package:system_status_platform_interface/src/Models/cpu_usage_info/cpu_usage_info_model.dart';
